d419071a10659f533165a29600b9b5eb28cec49f
optarou <optarou.fe@gmail.com>
2017-06-26 23:29:15 +0900

    ルーティングの設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IssueComponent } from './issue/issue.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WikiComponent } from './wiki/wiki.component';
+
+import { router, appRoutingProviders } from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IssueComponent,
+    PageNotFoundComponent,
+    WikiComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    router
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
