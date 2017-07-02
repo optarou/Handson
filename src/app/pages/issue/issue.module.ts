80c804565dcf3425c4ed78e08b61bc41f21b1e74
optarou <optarou.fe@gmail.com>
2017-07-02 23:01:52 +0900

    階層化されたModuleとRouter

--- src/app/pages/issue/issue.module.ts
+++ src/app/pages/issue/issue.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { IssueComponent } from "./issue.component";
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    IssueComponent
+  ]
 })
 export class IssueModule { }
--- src/app/pages/wiki/wiki.module.ts
+++ src/app/pages/wiki/wiki.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { WikiComponent } from "./wiki.component";
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    WikiComponent
+  ]
 })
 export class WikiModule { }
