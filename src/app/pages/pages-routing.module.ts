96b46447e3ab4ba1622196160389551e6716216e
optarou <optarou.fe@gmail.com>
2017-07-09 03:33:37 +0900

    ルーティングの設定

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PagesComponent } from './pages.component';
+import { TopComponent } from './top/top.component';
+import { IssueComponent } from './issue/issue.component';
+import { WikiComponent } from './wiki/wiki.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: '', redirectTo: 'top', pathMatch: 'full'},
+      { path: 'top', component: TopComponent },
+      { path: 'issue', component: IssueComponent },
+      { path: 'wiki', component: WikiComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
